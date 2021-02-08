e8e8f922f9b1b71d64884a3bfb585fda09e549a2
MykolaBerkosha <65126987+MykolaBerkosha@users.noreply.github.com>
2021-02-08 11:43:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_1
+{
+    public class Program
+    {
+        static public int Example(int a, int b = 60)
+        {
+            int c = a / b;
+            Console.WriteLine(a.ToString() + " / " + b + " = ~ " + c.ToString());
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть секунди");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            Example(a);
+            Console.ReadLine();
+        }
+    }
+}
